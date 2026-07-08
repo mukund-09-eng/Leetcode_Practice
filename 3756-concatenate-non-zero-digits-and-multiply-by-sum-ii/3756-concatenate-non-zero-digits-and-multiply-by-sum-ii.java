97e497b5033dda43bdc65d2ea521003cf41ab30f
mukund-09-eng <mukundbansal0001@gmail.com>
2026-07-08 15:22:22 +0530

    Time: 17 ms (86.67%), Space: 131.3 MB (23.33%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,45 +1,50 @@
 class Solution {
-    private static final int MOD = 1000000007;
-    private static final int MAX = 100001;
-    private static final int[] pow = new int[MAX];
-
-    static {
-        pow[0] = 1;
-        for (int i = 1; i < MAX; i++)
-            pow[i] = (int) ((pow[i - 1] * 10L) % MOD);
-    }
-
     public int[] sumAndMultiply(String s, int[][] queries) {
         int n = s.length();
-        int[] A = new int[n + 1];
-        int[] B = new int[n + 1];
-        int[] len = new int[n + 1];
+        int m = queries.length;
+        long MOD = 1000000007L;
+
+        long[] prefSum = new long[n + 1];
+        long[] prefVal = new long[n + 1];
+        int[] prefCnt = new int[n + 1];
+        long[] power = new long[n + 1];
+
+        power[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            power[i] = (power[i - 1] * 10) % MOD;
+        }
 
         for (int i = 0; i < n; i++) {
-            int d = s.charAt(i) - '0';            
-            A[i + 1] = A[i] + d;
-            
-            if (d > 0) {
-                B[i + 1] = (int) ((B[i] * 10L + d) % MOD);
-                len[i + 1] = len[i] + 1;
+            int d = s.charAt(i) - '0';
+
+            prefSum[i + 1] = prefSum[i] + d;
+            prefCnt[i + 1] = prefCnt[i] + (d != 0 ? 1 : 0);
+
+            if (d == 0) {
+                prefVal[i + 1] = prefVal[i];
             } else {
-                B[i + 1] = B[i];
-                len[i + 1] = len[i];
+                prefVal[i + 1] = (prefVal[i] * 10 + d) % MOD;
             }
         }
 
-        int[] res = new int[queries.length];
-        int i = 0;
+        int[] result = new int[m];
 
-        for (int[] q : queries) {
-            int l = q[0], r = q[1] + 1;
-            
-            long sub = ((long) B[l] * pow[len[r] - len[l]]) % MOD;
-            long x = (B[r] - sub + MOD) % MOD;
-            
-            res[i++] = (int) ((x * (A[r] - A[l])) % MOD);
+        for (int i = 0; i < m; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            int len = prefCnt[r + 1] - prefCnt[l];
+
+            long start = prefVal[l];
+            long end = prefVal[r + 1];
+
+            long x = (end - (start * power[len]) % MOD + MOD) % MOD;
+            long sum = prefSum[r + 1] - prefSum[l];
+
+            result[i] = (int) ((x * sum) % MOD);
         }
 
-        return res;
+        return result;
     }
 }
