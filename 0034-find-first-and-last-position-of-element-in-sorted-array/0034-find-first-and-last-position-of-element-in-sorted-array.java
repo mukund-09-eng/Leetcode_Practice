0ebcc86b8a0827caf0825666c8620e4365738a6b
mukund-09-eng <mukundbansal0001@gmail.com>
2026-07-08 14:20:24 +0530

    Time: 0 ms (100%), Space: 48 MB (86.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int [] ans = {-1,-1};
-       ans[0] = findfirst(nums,target);
-       ans[1] = findlast(nums,target);
-
-       return ans;
+        int [] ans = {-1,-1};
+        ans[0] = findFirst(nums,target);
+        ans[1] = findLast(nums,target);
+        return ans ;
     }
-
-       private int findfirst(int[] nums, int target){
-        int l =0;
-        int r = nums.length-1;
+    private int findFirst(int [] nums, int target){
         int first = -1;
+        int l = 0;
+        int r = nums.length -1;
         while(l<=r){
-            int mid = (l + r)/2;
-            if(nums[mid]== target){
-                first = mid ;
-                r = mid -1;
+            int mid = l+(r-l)/2;
+            if(nums[mid]>target){
+                r =  mid-1;
             }
-            else if(nums[mid]>target){
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                first = mid;
                 r = mid-1;
-
             }
-            else l = mid+1;
         }
         return first;
-       }
+    }
 
-       private int findlast(int[] nums, int target){
-        int l =0;
-        int r = nums.length-1;
+    private int findLast(int [] nums, int target){
         int last = -1;
+        int l = 0;
+        int r = nums.length -1;
         while(l<=r){
-            int mid = (l+r)/2;
-            if(nums[mid]==target){
-                last = mid;
-                l=  mid+1;
+            int mid = l+(r-l)/2;
+            if(nums[mid]>target){
+                r =  mid-1;
             }
-            else if(nums[mid]>target) r = mid-1;
-            else l = mid+1;
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                last = mid;
+                l =mid+1;
+            }
         }
         return last;
-       }
-
-        
     }
+}
