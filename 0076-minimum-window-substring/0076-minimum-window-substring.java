6c25bde4e33ce4306d677d318ab87bd55ea973a7
mukund-09-eng <mukundbansal0001@gmail.com>
2026-07-17 21:26:01 +0530

    Time: 2 ms (99.86%), Space: 44.9 MB (99.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,20 +1,20 @@
 class Solution {
     public String minWindow(String s, String t) {
-   
         if(s.length()<t.length()){
             return "";
         }
-
-        int [] freq = new int[128];
-        for(char c :t.toCharArray()){
+        int[] freq = new int[128];
+        for(char c:t.toCharArray()){
             freq[c]++;
-        } 
+        }
+
         int l =0;
         int st =0;
-        int min = Integer.MAX_VALUE;
+        int min =Integer.MAX_VALUE;
         int req = t.length();
         for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
+            
             if(freq[ch]>0){
                 req--;
             }
@@ -22,16 +22,16 @@ class Solution {
             while(req==0){
                 if((i-l+1)<min){
                     min = i-l+1;
-                    st = l;
+                    st =l;
                 }
-                char leftch = s.charAt(l);
-                freq[leftch]++;
-                if(freq[leftch]>0){
+                char lst = s.charAt(l);
+                freq[lst]++;
+                if(freq[lst]>0){
                     req++;
                 }
                 l++;
             }
         }
-        return min == Integer.MAX_VALUE ? "" : s.substring(st,st+min);
+        return min==Integer.MAX_VALUE ? "" : s.substring(st,min+st);
     }
 }
