2c825406248e14db12f9d86a0242c6a360913984
mukund-09-eng <mukundbansal0001@gmail.com>
2026-09-21 22:44:49 +0530

    Time: 0 ms (100%), Space: 42 MB (72.88%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 2;
+        if(n<=3) return n;
+        for(int i = 3;i<=n;i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
