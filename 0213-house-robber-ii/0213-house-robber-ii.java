746e0ab55e3ef83b943ac5ea06f0d66a2b08ca8a
mukund-09-eng <mukundbansal0001@gmail.com>
2026-09-22 12:00:10 +0530

    Time: 0 ms (100%), Space: 42.9 MB (22.82%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int case1 = robCase(nums,0,n-2);
+        int case2 = robCase(nums,1,n-1);
+        return Math.max(case1,case2);
+    }
+    private int robCase(int[] arr , int start,int end){
+        int prev2 =0;
+        int prev1 =0;
+        for(int i = start;i<=end;i++){
+            int take = arr[i]+prev2;
+            int skip = prev1;
+            int curr = Math.max(take,skip);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
