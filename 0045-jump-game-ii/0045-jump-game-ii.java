e83bc75ee2c03a980bd377d1c210e89fc1980364
mukund-09-eng <mukundbansal0001@gmail.com>
2026-09-23 13:55:41 +0530

    Time: 1 ms (99.79%), Space: 47.1 MB (75.97%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump =0;
+        int currentEnd = 0;
+        int far=0;
+        for(int i =0;i<nums.length-1;i++){
+            far = Math.max(far,i+nums[i]);
+            if(i==currentEnd){
+                jump++;
+                currentEnd = far;
+            }
+        }
+        return jump;
+    }
+}
